d449d27c2083f9472bcd7675281a9aee708820b0
catspie92@gmail.com <catspie92@gmail.com>
2024-02-28 19:50:43 +0900

    멤버 기능 구현

--- member-service/src/main/java/hello/memberService/web/basic/BasicMemberController.java
+++ member-service/src/main/java/hello/memberService/web/basic/BasicMemberController.java
@@ -33,14 +33,34 @@ public class BasicMemberController {
         return "basic/member";
     }
 
-    /** 사용자 등록 */
+    /** 사용자 등록 화면으로 이동 */
     @GetMapping("/add")
-    public String add() {
+    public String addForm() {
         return "basic/addForm";
     }
 
+    /** 사용자 등록 */
+    @PostMapping("/add")
+    public String save(@ModelAttribute Member member) {
+        memberRepository.save(member);
+        return "basic/addForm";
+    }
 
+    /** [파라미터값 넘기기 연습 필요] 사용자 수정 화면으로 이동 */
+    @GetMapping("/{memberId}/edit")
+    public String editForm(@PathVariable("memberId") long memberId, Model model) {
+        Member member = memberRepository.findById(memberId);
+        model.addAttribute("member", member);
+        return "basic/editForm";
+    }
 
+    /** [파라미터값 넘기기 연습 필요] 사용자 수정 */
+    @PostMapping("/{memberId}/edit")
+    public String edit(@PathVariable long memberId, @ModelAttribute Member member) {
+        memberRepository.update(memberId, member);
+        System.out.println("타긴 탐...");
+        return "redirect:/basic/members/{memberId}";
+    }
     /**
      * 테스트용 데이터 추가
      */
